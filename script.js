833ee02f8b3768d7516bca9a2b3cd1b23e3e8fc9
Jack Sarles <jack.sarles@mcmcg.com>
2024-03-01 12:21:57 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,9 @@ function Book() {
 
 function addBookToLibrary() {
   // do stuff here
+  // take user’s input and store the new book objects into an array. 
+   prompt('Enter the book title');
+    prompt('Enter the book author');
+    prompt('Enter the number of pages');
+
 }
