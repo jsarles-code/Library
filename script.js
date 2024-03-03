171406b9242a6b26d2b5c879fab5cbf19ec3cde8
Jack Sarles <57759984+jsarles-code@users.noreply.github.com>
2024-03-03 10:08:41 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//All of your book objects are going to be stored in an array, 
+// so add a function to the script (not the constructor)that can take user’s input and store the new book objects into an array.
+// Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
+// Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read and anything else you might want. 
+// Add a button on each book’s display to remove the book from the library.
+// Add a button on each book’s display to change its read status.
+
 const myLibrary = [];
 
 function Book() {
